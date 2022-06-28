8591ffb29f69295da731423bc0be4cfdea49e232
Maksim198505 <86347406+Maksim198505@users.noreply.github.com>
2022-06-28 09:41:36 +0300

    Revision of the program

--- zadanie_5.py
+++ zadanie_5.py
@@ -2,25 +2,24 @@
 # предел (limit), после чего попробуйте сгенерировать по порядку все числа.
 # Меньшие значения предела, которые имеют все и только простые множители простых чисел primesL.
 
+from functools import reduce as red
+
+
 def count_find_num(primesL, limit):
-    global n
-    n = limit
-    i = 2
-    prim_fac = []
-    while i * i <= n:
-        while n % i == 0:
-            prim_fac.append(i)
-            n = n / i
-        i = i + 1
-    if n > 1:
-        prim_fac.append(n)
-    return prim_fac
-
-
-primesL = [2, 5]
-limit = 200
-val = count_find_num(primesL, limit) == [8, 200]
+    if red(lambda a, b: a * b, primesL) > limit:
+        return []
+    result = list()
+    result.append(red(lambda a, b: a * b, primesL))
+    for i in primesL:
+        for prim_fac in result:
+            prim_fac *= i
+            while prim_fac <= limit and prim_fac not in result:
+                result.append(prim_fac)
+                prim_fac *= i
+    return [len(result), max(result)]
 
 
 if __name__ == '__main__':
+    primesL = list(map(int, input().split()))
+    limit = int(input())
     print(count_find_num(primesL, limit))
